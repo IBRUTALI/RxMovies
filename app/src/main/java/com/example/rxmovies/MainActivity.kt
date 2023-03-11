c25aa051bad000c3f87d0a71ef0a30d6c857d9c8
Igor Osipov <ighor.osipov.95@mail.ru>
2023-03-11 13:50:43 +0300

    Added splash screen and toolbar menu.

--- app/src/main/java/com/example/rxmovies/MainActivity.kt
+++ app/src/main/java/com/example/rxmovies/MainActivity.kt
@@ -1,22 +1,68 @@
 package com.example.rxmovies
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.rxmovies.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
-    lateinit var navController: NavController
+    private var mBinding: ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupNavigation()
+    }
+    private fun setupNavigation() {
         navController = Navigation.findNavController(this, R.id.nav_host)
-        setupActionBarWithNavController(navController)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.mainFragment
+            )
+        )
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+        binding.toolbar.inflateMenu(R.menu.main_menu)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.splashFragment) {
+                binding.toolbar.visibility = View.GONE
+            } else {
+                binding.toolbar.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (navController.currentDestination?.id == R.id.mainFragment) {
+            finish()
+        } else {
+            navController.popBackStack()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || onSupportNavigateUp()
+        return if (binding.root.findNavController().currentDestination?.id != R.id.mainFragment)
+            navController.navigateUp() || onSupportNavigateUp()
+        else false
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mBinding = null
     }
 }
--- app/src/main/java/com/example/rxmovies/screens/splash/SplashFragment.kt
+++ app/src/main/java/com/example/rxmovies/screens/splash/SplashFragment.kt
@@ -1,18 +1,59 @@
 package com.example.rxmovies.screens.splash
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.rxmovies.R
+import com.example.rxmovies.databinding.FragmentSplashBinding
+import io.reactivex.Completable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class SplashFragment : Fragment() {
+    private var mBinding: FragmentSplashBinding? = null
+    private val binding get() = mBinding!!
+
+    private fun init() {
+        val progress = progressAnimation()
+            .subscribeOn(AndroidSchedulers.mainThread())
+            .delay(1000, TimeUnit.MILLISECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                binding.root.findNavController().navigate(R.id.action_splashFragment_to_mainFragment)
+            }, {
+
+            })
+    }
+
+    private fun progressAnimation(): Completable {
+        return Completable.create {subscriber ->
+            binding.splashProgress.max = 20
+            val value = 20
+            ObjectAnimator.ofInt(binding.splashProgress, "progress", value).setDuration(1000).start()
+            subscriber.onComplete()
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+    ): View {
+        mBinding = FragmentSplashBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mBinding = null
     }
 }
